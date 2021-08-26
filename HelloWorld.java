bc3b249ce7c6011d7343539ffa0ed9b212a2fb4c
teacher Marcos <teacher.mrdv@gmail.com>
2021-08-26 12:18:15 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,12 @@
+/*
+ * Hello world 26 Aug 2021
+ * IBS1 Comp Sci
+ */
+
 public class HelloWorld
 {
-    
+	public static void main(String[] args)
+	{
+		System.out.println("Hello Grade 11 2021!!!");
+	}
 }
