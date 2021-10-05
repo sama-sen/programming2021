8fa8c54edc534382f7dfdfe97edc3c457f0f1655
mrDV <48473242+teacher-mrdv@users.noreply.github.com>
2021-10-05 12:30:29 +0800

    Update InputWOIBIO.java

--- InputWOIBIO.java
+++ InputWOIBIO.java
@@ -1,6 +1,5 @@
 /*
  * Input without IBIO example
- * 
  */
 import java.util.*;
 
@@ -13,12 +12,15 @@ public class InputWOIBIO
 		System.out.print("Input an integer: ");
 		int number = in.nextInt();
 		System.out.println("Input number was " + number);
+		
 		System.out.print("Input a double: ");
 		double decimal = in.nextDouble();
 		System.out.println("Input number was " + decimal);
-		String s = in.nextLine();
+		
+		String s = in.nextLine(); // only needed because we previously input a number
 		System.out.print("Input a String: ");
 		s = in.nextLine();
+		
 		System.out.println("The string was " + s);
 		in.close();
 	}
