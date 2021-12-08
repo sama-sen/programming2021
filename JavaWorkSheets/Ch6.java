08f233f01e384f7ee28deb485e34ae21bb97bb32
teacher Marcos <teacher.mrdv@gmail.com>
2021-12-08 14:46:20 +0800

    HL update

---
+++ JavaWorkSheets/Ch6.java
@@ -0,0 +1,40 @@
+/*
+ * Chapter 6 example
+ * 
+ */
+
+
+public class Ch6
+{
+	// returns a String with howMany number of *
+	public static String stars(int howMany)
+	{
+		String s = ""; // initialise String s to empty
+		for(int i = 0; i < howMany; i++)
+		{
+			s = s + "*";
+		}
+		return s;
+	}
+	
+	
+	public static void main (String[] args)
+	{
+		int num = IBIO.inputInt("enter number of lines ");
+		String line = stars(num);
+		for (int i = 0; i < num; i++)
+		{
+			IBIO.output(line);
+	}
+}
+
+/*
+
+enter number of lines 5
+*****
+*****
+*****
+*****
+*****
+
+*/
