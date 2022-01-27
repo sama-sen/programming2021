6228dda6d5e8843c279d9f8e8e1fcc272679bbf8
mrDV <48473242+teacher-mrdv@users.noreply.github.com>
2022-01-27 11:30:02 +0800

    up

---
+++ StringDemo.java
@@ -0,0 +1,20 @@
+/*
+ * StringDemo.java
+ * 
+ */
+
+
+public class StringDemo
+{
+	
+	public static void main (String[] args)
+	{
+		String salutation = "Ms";
+		String givenName  = "Presidenta";
+		String surname    = "OFS";
+		salutation = salutation.charAt(0) + "is" + salutation.charAt(1);
+		System.out.
+		println( salutation + " " + givenName + " " + surname);
+	}
+}
+
