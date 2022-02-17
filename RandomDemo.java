37c33c7727d56e91e99f741c3a6f9cf91cc638c5
mrDV <48473242+teacher-mrdv@users.noreply.github.com>
2022-02-17 13:20:14 +0800

    thursday update

---
+++ RandomDemo.java
@@ -0,0 +1,15 @@
+
+
+public class RandomDemo
+{
+	
+	public static void main (String[] args)
+	{
+		for(int i = 0; i < 10; i++)
+		{	System.out.println(Math.random());
+			System.out.println(Math.random()*6);
+		}
+		System.out.println("BYEBYE");
+	}
+}
+
