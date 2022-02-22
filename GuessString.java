c52094089278305c8db26c35cf0e862358524342
teacher Marcos <teacher.mrdv@gmail.com>
2022-02-22 12:20:29 +0800

    Create GuessString.java

---
+++ GuessString.java
@@ -0,0 +1,43 @@
+/*
+ * Guess the string
+ * 
+ * 
+ */
+
+
+public class GuessString
+{
+	
+	public static void printArray(char[] a)
+	{
+		for(int index = 0; index < a.length; index++)
+		{
+			System.out.println(index + " : " + a[index]);
+		}
+	}
+	
+	public static void main (String[] args)
+	{
+		String guessMe = "a";
+		char[] letters = new char[26];
+		int i = 0;
+		for (char c = 'a'; c <= 'z'; c++)
+		{
+			letters[i] = c;
+			i++;
+		}
+		//printArray(letters);
+		
+		char x = guessMe.charAt(0);
+		for(int n = 0; n < letters.length; n++)
+		{
+			System.out.print( letters[n] );
+			if( letters[n] == x )
+			{	System.out.println( " <<< " );
+				break;
+			}
+		}
+		
+	}
+}
+
