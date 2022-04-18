73ec73d0df064e174411bb569e92687f7b66e040
teacher Marcos <teacher.mrdv@gmail.com>
2022-04-18 17:51:20 +0800

    Update LinkedListDemo.java

--- HL/ADTs/LinkedListDemo.java
+++ HL/ADTs/LinkedListDemo.java
@@ -18,6 +18,21 @@ public class LinkedListDemo
 		list1.append("Mary"); list1.printList();
 		list1.append("Peter"); list1.printList();
 		list1.append("Bruce"); list1.printList();
+		list1.append("Jane"); list1.printList();
+		/* work in progress - uncomment as you complete
+		 * the delete and insert methods
+		list1.delete("Mary"); list1.printList();
+		list1.delete("Ollie"); list1.printList();
+		list1.delete("Jane"); list1.printList();
+		* end of delete test cases
+		list1.insertAfter("Bruce", "Diana"); list1.printList();
+		list1.insertAfter("Lisa", "Ada"); list1.printList();
+		list1.insertAfter("Peter", "Otto"); list1.printList();
+		* end of insertAfter test cases
+		* 
+		* special challenge --if you have time--
+		* write and test an insertBefore method!
+		*/
 	}
 }
 
