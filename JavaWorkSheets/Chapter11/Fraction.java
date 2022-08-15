94fc2d571027b88223109bb65f1f4fc321be7d29
teacher Marcos <teacher.mrdv@gmail.com>
2022-08-15 16:36:19 +0800

    Monday update

---
+++ JavaWorkSheets/Chapter11/Fraction.java
@@ -0,0 +1,24 @@
+/*
+ * Fraction.java
+ * 
+ */
+
+
+public class Fraction
+{
+	private int num;
+	private int den;
+	
+	public Fraction() { } // empty constructor
+	
+	public Fraction(int num, int den) // custom constructor
+	{
+		this.num = num;
+		this.den = den;
+	}
+	// these constructors are overloaded
+	
+
+
+}
+
---
+++ JavaWorkSheets/Chapter11/FractionTest.java
@@ -0,0 +1,14 @@
+
+public class FractionTest
+{
+	
+	// main method for quick testing and demo purposes only
+	public static void main(String[] args)
+	{
+		Fraction f = new Fraction();
+		Fraction g = new Fraction(1, 2);
+		System.out.println(f.num);
+		System.out.println(g.den);
+	}
+}
+
